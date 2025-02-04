5d6a51e83268f31a88576e33533774df616260f5
felipeuba <ubafelipe005@gmail.com>
2025-02-04 18:41:48 -0300

    added restrictions to Person class

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.use(express.json())
 app.use(morgan('tiny'))
 
 mongoose.set('strictQuery',false)
-mongoose.connect(url)
 
 morgan.token('body', (req) => {
   if (req.method === 'POST') {
@@ -35,8 +34,8 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const now = new Date();
-const time = now.toString();
+// const now = new Date();
+// const time = now.toString();
 // const info = `
 // <p>Phonebook has info for ${people.length} people </p>
 // <p>${time}</p>
@@ -52,53 +51,70 @@ app.get('/api/people', (request, response) => {
   })
 })
 
-app.get('/api/people/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = people.find(person => person.id === id)
-    if (person) {
+app.get('/api/people/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+
+    .catch(error => next(error))
 })
 
-app.delete('/api/people/:id', (request, response) => {
-    const id = Number(request.params.id)
-    people = people.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/people/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () =>{
-    return Math.floor(Math.random() * 999999)
+app.put('/api/people/:id', (request, response, next) => {
+  const {name, number} = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    {name, number},
+      { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/people', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+
+    .catch(error => next(error))
+})
+
+//error handlers
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
-app.post('/api/people', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-  
-    if (people.find(p => p.name === body.name)) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-  
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }
-  
-    console.log(person);
-    
-    people = people.concat(person)
-
-    response.json(person)
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
